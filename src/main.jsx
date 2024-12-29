714ffebd06b9ae503bcd8c50267736603b493462
r1d3rzz <myintthwaykhine127@gmail.com>
2024-12-29 22:03:27 +0630

    deploy fix: remote app.css file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.jsx";
-import "./app.css";
 import "../node_modules/flowbite/dist/flowbite.min.js";
 
 createRoot(document.getElementById("root")).render(
